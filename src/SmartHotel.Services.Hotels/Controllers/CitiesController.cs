822b41252d389ebb1692675d375dac9a933b18b2
Stephen Provine <stephpr@microsoft.com>
2018-05-07 16:21:04 -0700

    Fix keynote bug, move to azds

--- src/SmartHotel.Services.Hotels/Controllers/CitiesController.cs
+++ src/SmartHotel.Services.Hotels/Controllers/CitiesController.cs
@@ -23,8 +23,7 @@ namespace SmartHotel.Services.Hotels.Controllers
         {
             var cities = string.IsNullOrEmpty(name) ? 
                 await _citiesQueries.GetDefaultCities() :
-                _citiesQueries.GetDefaultCities().Result
-                .Where(city => city.Name.StartsWith(name));
+                await _citiesQueries.Get(name);
             return Ok(cities);
         }
     }
